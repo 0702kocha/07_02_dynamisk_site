e0bff695f3be2a6752f5437cfe50d6d92f12c83f
c_zino <celinakhrs@gmail.com>
2025-02-14 10:33:36 +0100

    update

--- js/productslist.js
+++ js/productslist.js
@@ -3,7 +3,7 @@ const mycategory = new URLSearchParams(window.location.search).get("category");
 
 // Finder de html elementer der skal bruges i js
 const ListContainer = document.querySelector(".product_list_container");
-const overskrift = document.querySelector("h1")
+const overskrift = document.querySelector("h1");
 
 // Sørger for at vores overskrift viser den valgte kategori
 overskrift.innerHTML = mycategory;
@@ -20,22 +20,33 @@ function showProducts(data) {
   const markup = data.products
     .map(
       (product) => `
-      <article class="product_container">
-        <div class="image-container">
-          <img
-            src="${product.images[1] || product.images[0]}"
-            alt="Billede af produkt"
-          />
-        </div>
-        <div>
-          <h3 class="productdisplayname">${product.title}</h3>
-          <p class="brandname">${product.brand}</p>
-          <h4 class="price">${product.price} $</h4>
-          <div class="button">
-            <a href="produkt.html?id=${product.id}">Read More</a>
+         
+        <article class="product_container">
+      
+          <div class="image-container">
+          <a href="produkt.html?id=${product.id}" class="product_link">
+            <img
+              src="${product.images[1] || product.images[0]}"
+              alt="Billede af produkt"
+            />
+           <p class="discount ${product.discountPercentage > 0 && "IsOnSale"}">-${product.discountPercentage}%</p>
+            <p class="sold-out ${product.stock <= 0 && "IsSoldOut"}">Sold Out</p>
+          
+            </a>
           </div>
-        </div>
-      </article>
+          <div>
+            <a href="produkt.html?id=${product.id}" class="product_link">
+            <h3 class="productdisplayname">${product.title}</h3>
+            <p class="brandname">${product.brand}</p>
+   <p class="price ${product.discountPercentage > 0 ? "discount-price IsOnSale" : ""}">${product.price} $</p>
+            ${product.discountPercentage > 0 ? 
+              `<p class="discount-price IsOnSale">Nu DKK ${Math.round(product.price * (1 - product.discountPercentage / 100))},-</p>` 
+              : ""}
+       
+            </a>
+          </div>
+        </article>
+  
       `
     )
     .join("");
@@ -45,7 +56,3 @@ function showProducts(data) {
   // Indsætter det i det færdige html i elementet
   ListContainer.innerHTML = markup;
 }
-
-
-
-
