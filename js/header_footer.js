92bcd3b7af4a1afcacde05988ab6b9904310054f
MariaXiaEgedal <mariaegedal@gmail.com>
2025-02-15 13:43:18 +0100

    laver på header

---
+++ js/header_footer.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const header = document.querySelector("header");
+    const burgerToggle = document.getElementById("burger-toggle");
+  
+    // Når brugeren klikker på burger-menuen
+    burgerToggle.addEventListener("change", function () {
+      if (this.checked) {
+        header.classList.add("menu-open");
+      } else {
+        header.classList.remove("menu-open");
+      }
+    });
+  
+    // Gør også at header får baggrund når man scroller
+    window.addEventListener("scroll", function () {
+      if (window.scrollY > 10) {
+        header.classList.add("scrolled");
+      } else {
+        header.classList.remove("scrolled");
+      }
+    });
+  });
+  
